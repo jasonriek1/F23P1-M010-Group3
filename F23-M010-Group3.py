3a4f06a7e9251feefb1fd611c35488491b169034
jasonriek1 <145682301+jasonriek1@users.noreply.github.com>
2023-09-28 08:47:46 -0400

    Update F23-M010-Group3.py

--- F23-M010-Group3.py
+++ F23-M010-Group3.py
@@ -1,3 +1,4 @@
+import pandas as pd
 #task 1 
 
 
@@ -16,3 +17,14 @@
 
 
 #task 6
+#Returns true if both text files are equal
+#opens both files and puts them into a variable
+#evaluates both to see if they are equal in the return statement
+def areFilesEqual(fileName1, fileName2): #files names must be inputed in double qoutes. ex. areFilesEqual("placeholder1.txt", "placeholder2.txt")
+    f = open(fileName1) #opens file one
+    s1 = f.read() #turns contents of file one into string
+    f.close()
+    f = open(fileName2) #opens file two
+    s2 = f.read() #turns contents of file two into string
+    f.close()
+    return s2==s1 #true if equal false otherwise
