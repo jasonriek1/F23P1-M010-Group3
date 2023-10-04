952a4073cc67a32597978afa13909b78f6d2e952
jasminepdilla <145682296+jasminepdilla@users.noreply.github.com>
2023-10-03 22:51:56 -0400

    Update F23-M010-Group3.py
    Tasks 2 and 3
    String to binary and binary to string functions

--- F23-M010-Group3.py
+++ F23-M010-Group3.py
@@ -4,9 +4,70 @@ import pandas as pd
 
 
 #task 2
+'''
+This function takes a string and checks the length of the string 
+and then checks to to see if it  matches anything in the chars list
+Then, uses it's index to find the corresponding binary
+'''
+def string_to_binary(p1: str):
+    # checks for char of length 3, we have 3 in our excel file
+    if len(p1) >= 3:
+        working_string = p1[0:3] # takes first 3 characters
+        for i in range(len(chars)):
+            if chars[i] == working_string: # if we have those 3 characters in our character list
+                p1 = p1[3:] # cut out the first 3 chars
+                bin_value = bins[i] # find corresponding bin value
+                return bin_value, p1
+
+    # checks for char of length 2, we have 4 in our excel file
+    if len(p1) >= 2:
+        working_string = p1[0:2] # we only care about the first two characters
+        for i in range(len(chars)):
+            if chars[i] == working_string: # if we have those 3 characters in our character list
+                p1 = p1[2:] # take out the first 2 characters we were working with
+                bin_value = bins[i] # find corresponding bin value
+                return bin_value, p1
+
+    # checks for char of length 1, ie. single letters, numbers, or punctuation
+    if len(p1) >= 1:
+        working_string = p1[0:1]
+        for i in range(len(chars)):
+            if chars[i] == working_string:
+                if len(p1) > 1:
+                    p1 = p1[1:]
+                else: p1 = ''
+                bin_value = bins[i]
+                return bin_value, p1
+
+    # if no character, returns blank string + blank binary
+    return '', ''
+
 
 
 #task 3
+# this function cuts apart the input into either the short or long bits
+def first_binary(p1: str):
+    flag = p1[0] # checks whether it's a short character or long (short start w/ 0, long w/ 1)
+    if flag == 0:
+        # short characters = 5 bits
+        working_bin = p1[0:5] # cuts out the first 5 bits
+        p1 = p1[5:] # this is what is left
+    else:
+        # long characters = 7 bits
+        working_bin = p1[0:7]
+        p1 = p1[7:]
+
+    return working_bin, p1
+
+
+# this function takes the binary numer we cut out and uses its index from
+# our list to match it to the corresponding char
+def binary_to_string(p1: str):
+    i = bins.index(p1)
+    return chars[i]
+
+
+#task 4
 def func4(p1: str):
     #opens the file and read in values and then closes it
     o = open(p1)
@@ -34,6 +95,8 @@ def func4(p1: str):
     opening.write(writing)
     opening.close()
 
+
+# task 5
 def func5(p1 = "BinOutput.txt"):
     #opening and reading in the file
     o = open(p1)
